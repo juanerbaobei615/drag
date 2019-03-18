9fc9c569af6d7eaa72342047d01e7fe6f56d1868
ghostmb <337924752@qq.com>
2019-03-18 20:00:16 +0800

    drag.js修改

---
+++ drag.js
@@ -0,0 +1,9 @@
+<script>
+window.onload = function () {
+        var oUl = document.getElementById("list");
+        var aH2 = oUl.getElementsByTagName("h2");
+        var aUl = oUl.getElementsByTagName("ul");
+        var aLi = null;
+        var arrLi = [];
+    }
+</script>
